3995e3130b50d133c2213325b4e5b3bf9dc91254
juhawkin <julie.hawkins@gov.bc.ca>
2019-02-27 09:26:38 -0800

    Used once (with REGNAMES_from_Access.csv) to create lookup for region names

---
+++ analysis/make_lookup.R
@@ -0,0 +1,35 @@
+## load libraries
+library(tidyverse)
+library(here)
+
+## pre-specify column types
+col_specs <- cols(.default = col_integer(), Region.Type = col_character())
+
+## read in data
+lookup <- readr::read_csv(here("app","data",paste0("FromR1.csv")), col_names = TRUE, col_specs)
+
+## change Region.Type to names
+lookup <- lookup %>% 
+  group_by(Region, Region.Type) %>% 
+  tally() %>% 
+  select(-n)
+
+# ## check
+# a <- lookup %>% group_by(Region.Type) %>% tally()
+# sum(a$n)  ## 280, but Access database has only 255 - the difference is in SR - only 7 in Access
+# rm(a)
+
+## read in REGNAMES_from_Access.csv to match up
+## REGNAMES_from_Access.csv was created by opening Database work/WorkingFile.accdb and copying REGNAMES into Excel
+REGNAMES <- read_csv(here("analysis", "REGNAMES_from_Access.csv"))
+
+## add Region.Names
+lookup <- left_join(lookup, REGNAMES, by = c("Region" = "TYPEID", "Region.Type" = "TYPE"))
+## NAs are due to fact that REGNAMES has names for only 7 in SR Region.Type
+
+## save as csv
+write_csv(lookup, here("app","data","lookup.csv"))
+
+## clean-up
+rm(col_specs, lookup, REGNAMES)
+
