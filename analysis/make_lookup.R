1d6679941333f1d6a3b6baecced41b19d2c773c7
Hawkins email = Julie.Hawkins@gov.bc.ca <Julie.Hawkins@gov.bc.ca>
2019-05-23 11:46:14 -0700

    Update to work with new data locations/being sourced in get_data.R script.

--- analysis/make_lookup.R
+++ analysis/make_lookup.R
@@ -1,20 +1,42 @@
 ## load libraries
+if (!require('here')) install.packages('here')
+if (!require('tidyverse')) install.packages('tidyverse')
+
 library(tidyverse)
 library(here)
 
-## pre-specify column types
-col_specs <- cols(.default = col_integer(), Region.Type = col_character())
 
-## read in data
-lookup <- readr::read_csv(here("analysis", "inputs", paste0("FromR1.csv")), col_names = TRUE, col_specs)
+## if df doesn't exist in global environment, get it
+if(!("df" %in% ls(.GlobalEnv))){
+  
+  ## if csv file_type, else xlsx file_type
+  if(file_type == "csv"){
+    
+    ## read in csv data
+    df <- readr::read_csv(file = paste0(base_folder, file_name, age_var, ".csv"))
+    
+  } else {
+    
+    ## read in xlsx data
+    df <- xlsx::read.xlsx2(file = paste0(base_folder, file_name, age_var, ".xlsx"),
+                           sheetName = mysheet, stringsAsFactors = FALSE) %>%
+      mutate_at(vars(-c(data_cols[1])), as.numeric)  ## else everything comes in as character class
+  }
+  
+  ## change Region.Type and Region colnames if needed
+  if(data_cols[1] != final_cols[1]){ df <- rename(df, !!final_cols[1] := !!data_cols[1]) }  ## "Region.Type"
+  if(data_cols[2] != final_cols[2]){ df <- rename(df, !!final_cols[2] := !!data_cols[2]) }  ## "Region"
+
+}
+
 
 ## change Region.Type to names
-lookup <- lookup %>% 
+lookup <- df %>% 
   group_by(Region, Region.Type) %>% 
   tally() %>% 
   select(-n)
 
-# ## check
+# ## check (on 2017 data)
 # a <- lookup %>% group_by(Region.Type) %>% tally()
 # sum(a$n)  ## 280, but Access database has only 255 - the difference is in SR - only 7 in Access
 # rm(a)
@@ -31,5 +53,5 @@ lookup <- left_join(lookup, REGNAMES, by = c("Region" = "TYPEID", "Region.Type" 
 write_csv(lookup, here("analysis", "inputs", "lookup.csv"))
 
 ## clean-up
-rm(col_specs, lookup, REGNAMES)
+rm(lookup, REGNAMES)
 
