a42456b21cf2677308a3cafea0293dde8a36ff07
Lavoie <Sebastien.Lavoie@gov.bc.ca>
2019-10-23 12:04:20 -0700

    Update app to BCGov requirement. Embed BCSans font, improve accessibility.

--- app/app.R
+++ app/app.R
@@ -16,97 +16,130 @@ if (!require('tidyverse')) install.packages('tidyverse')
 if (!require('shiny')) install.packages('shiny')
 if (!require('shinydashboard')) install.packages('shinydashboard')
 if (!require('rsconnect')) install.packages('rsconnect')
+if (!require('DT')) install.packages('DT')
+
 
 ## read data ----
 data1 <- readRDS("data/data1.rds")  ## by single-year intervals
 
 # UI demonstrating column layouts
-ui <- fluidPage(
+ui <- fluidPage(title = "BC Population Estimates",
+  theme = "bootstrap.css",
+  HTML("<html lang='en'>"),
   fluidRow(
     column(width = 12, 
            style = "background-color:#003366; border-bottom:2px solid #fcba19;",
-           
            tags$header(class="header", style="padding:0 0px 0 0px; display:flex; height:80px; width:100%;",
              tags$div(class="banner", style="display:flex; justify-content:flex-start; align-items:center; margin: 0 10px 0 10px",
                a(href="https://www2.gov.bc.ca/gov/content/data/about-data-management/bc-stats",
-                 img(src = "bcstats_logo_rev.png", title = "BC Stats", height = "80px")
+                 img(src = "bcstats_logo_rev.png", title = "BC Stats", height = "80px", alt = "British Columbia - BC Stats")
                ),
-               h4("British Columbia - Population Estimates", style="font-family:`Noto Sans`; font-weight:400; font-size:3vmin; color:white; margin: 5px 5px 0 18px;")
+               h1("British Columbia - Population Estimates", style="font-weight:400; color:white; margin: 5px 5px 0 18px;")
              )
            )
             
     ),
+    column(width=12,
+            tags$fieldset(
+                  tags$legend(h2("How to use the population estimates application")),
+                  p("Select a region type, and then the region(s), year(s) and gender(s) of interest.
+                  Use the Ctrl and/or Shift keys to select multiple entries. Then select whether you'd
+                  like results by single year of age, 5-year age groups, totals, or your own custom age groupings. If you
+                  would like to specify your own custom age groups, select 'Custom Age Groups' and
+                  enter them in the boxes to the right as seen in the example below. Then click
+                  'Generate output'. You can view the results on screen or download a CSV file.",
+                  style="font-size:14px; color:#494949"),
+                  br()
+            )
+    ),
     column(width = 12,
            sidebarLayout(
-             sidebarPanel(style="background-color:#F2F2F2",
-               tags$em(helpText("This interactive tool allows you to select the region(s), year(s) and sex(es)
-                                 of interest for age groupings of your choice.",
-                                style="font-size:14px; font-family:`Noto Sans`; margin-left:10px; margin-top:10px; display:block; color:#494949")),
-               uiOutput("Region.Type"),
-               uiOutput("Region.Name"),
-               uiOutput("Year"),
-               uiOutput("Gender")
+             sidebarPanel(style="background-color:#F2F2F2;",
+               tags$fieldset(
+                 tags$legend(h3("Data selection")),
+                 uiOutput("Region.Type"),
+                 uiOutput("Region.Name"),
+                 uiOutput("Year"),
+                 uiOutput("Gender")
+               )
              ),
              mainPanel(
-               fluidRow(
-                       box(
-                           helpText("Select a region type, and then the region(s), year(s) and sex(es) of interest.
-                                Use the Ctrl and/or Shift keys to select multiple entries. Then select whether you'd
-                                like results by single year of age, 5-year age groups, totals, or your own custom age groupings. If you
-                                would like to specify your own custom age groups, select 'Custom Age Groups' and
-                                enter them in the boxes to the right as seen in the example below. Then click
-                                'Generate output'. You can view the results on screen or download a CSV file.",
-                                 style="font-size:14px; font-family:`Noto Sans`; color:#494949"),
-                           column(width = 3, br(),
-                                  uiOutput("Age_Type")
-                                  ),  ## end of column
-                           column(width = 3, br(),
-                                  tags$b("Custom age groups example:"),
-                                  tableOutput(outputId = "example_table")
-                                  ),  ## end of column
-                            fluidRow(
-                              tags$b("Custom Age Types:"),
-                              br(),
-                              column(width = 3,
-                                        tags$b("From"),
-                                        numericInput(inputId = "start1", label = NULL, value = NA, min = 0, max = 99),
-                                        numericInput(inputId = "start2", label = NULL, value = NA, min = 0, max = 99),
-                                        numericInput(inputId = "start3", label = NULL, value = NA, min = 0, max = 99),
-                                        numericInput(inputId = "start4", label = NULL, value = NA, min = 0, max = 99),
-                                        numericInput(inputId = "start5", label = NULL, value = NA, min = 0, max = 99)
-                                        ), ## end of column (starts)
-                                 column(width = 3,
-                                        tags$b("To"),
-                                        numericInput(inputId = "end1", label = NULL, value = NA, min = 0, max = 99),
-                                        numericInput(inputId = "end2", label = NULL, value = NA, min = 0, max = 99),
-                                        numericInput(inputId = "end3", label = NULL, value = NA, min = 0, max = 99),
-                                        numericInput(inputId = "end4", label = NULL, value = NA, min = 0, max = 99),
-                                        numericInput(inputId = "end5", label = NULL, value = NA, min = 0, max = 99)
-                                        ) ## end of column (ends)
-                             ),  ## end of fluidRow
-                           actionButton(inputId = "goButton", label = "Generate output"),
-                           actionButton(inputId = "resetButton", label = "Reset selection"),
-                           downloadButton(outputId = "downloadData", label = "Download data as csv"),
-                           br(),br(),
-                           width = 12,
-                           dataTableOutput(outputId = "table")
-                           )  ## end of box; boxes need to be put in a row (or column)
-                       )  ## end of fluidRow
+               tags$fieldset(
+                 style = "margin-top:20px;",
+                 tags$legend(h3("Age format selection")),        
+                 column(width=12,
+                             column(width = 3,
+                                    tags$fieldset(
+                                      tags$legend(h4("Select type of age group")),
+                                      uiOutput("Age_Type")
+                                    )
+                                    ),  ## end of column
+                             column(width = 3, 
+                                    tags$fieldset(
+                                      tags$legend(h4("Custom age groups example")),
+                                      tableOutput(outputId = "example_table")
+                                    )
+                                    ),  ## end of column
+                             
+                            column(width=6,     
+                                tags$fieldset(
+                                  tags$legend(h4("Custom age groups")),
+                                br(),
+                                column(width = 6,
+                                          numericInput(inputId = "start1", label = "From - 1st", value = NA, min = 0, max = 99)
+                                          ), 
+                                column(width = 6,
+                                          numericInput(inputId = "end1", label = "To - 1st", value = NA, min = 0, max = 99)
+                                          ), 
+                                column(width = 6,
+                                       numericInput(inputId = "start2", label = "From - 2nd", value = NA, min = 0, max = 99)
+                                ), 
+                                column(width = 6,
+                                       numericInput(inputId = "end2", label = "To - 2nd", value = NA, min = 0, max = 99)
+                                ), 
+                                column(width = 6,
+                                       numericInput(inputId = "start3", label = "From - 3rd", value = NA, min = 0, max = 99)
+                                ), 
+                                column(width = 6,
+                                       numericInput(inputId = "end3", label = "To - 3rd", value = NA, min = 0, max = 99)
+                                ), 
+                                column(width = 6,
+                                       numericInput(inputId = "start4", label = "From - 4th", value = NA, min = 0, max = 99)
+                                ),
+                                column(width = 6,
+                                       numericInput(inputId = "end4", label = "To - 4th", value = NA, min = 0, max = 99)
+                                )
+                              )  ## end of fluidRow
+                            )
+                    )
+                  ),  ## end of fluidRow
+               br(),
+                 tags$fieldset(
+                   tags$legend(h3("Actions")),
+                   column(width=12,
+                          actionButton(inputId = "goButton", label = "Generate output"),
+                          actionButton(inputId = "resetButton", label = "Reset selection"),
+                          downloadButton(outputId = "downloadData", label = "Download data as csv")
+                   )
+                 ),
+                 br(),br(),
+                 DTOutput("table"),
+                 br()
              )
            )
     ),
     column(width = 12,
            style = "background-color:#003366; border-top:2px solid #fcba19;",
            
-            tags$footer(class="footer", style="font-family:`Noto Sans`;",
+            tags$footer(class="footer",
               tags$div(class="container", style="display:flex; justify-content:center; flex-direction:column; text-align:center; height:46px;",
                 tags$ul(style="display:flex; flex-direction:row; flex-wrap:wrap; margin:0; list-style:none; align-items:center; height:100%;",
-                  tags$li(a(href="https://www2.gov.bc.ca/gov/content/home", "Home", style="font-size:1em; font-weight:normal; font-family:`Noto Sans`; color:white; padding-left:5px; padding-right:5px; border-right:1px solid #4b5e7e;")),
-                  tags$li(a(href="https://www2.gov.bc.ca/gov/content/home/disclaimer", "Disclaimer", style="font-size:1em; font-weight:normal; font-family:`Noto Sans`; color:white; padding-left:5px; padding-right:5px; border-right:1px solid #4b5e7e;")),
-                  tags$li(a(href="https://www2.gov.bc.ca/gov/content/home/privacy", "Privacy", style="font-size:1em; font-weight:normal; font-family:`Noto Sans`; color:white; padding-left:5px; padding-right:5px; border-right:1px solid #4b5e7e;")),
-                  tags$li(a(href="https://www2.gov.bc.ca/gov/content/home/accessibility", "Accessibility", style="font-size:1em; font-weight:normal; font-family:`Noto Sans`; color:white; padding-left:5px; padding-right:5px; border-right:1px solid #4b5e7e;")),
-                  tags$li(a(href="https://www2.gov.bc.ca/gov/content/home/copyright", "Copyright", style="font-size:1em; font-weight:normal; font-family:`Noto Sans`; color:white; padding-left:5px; padding-right:5px; border-right:1px solid #4b5e7e;")),
-                  tags$li(a(href="https://www2.gov.bc.ca/StaticWebResources/static/gov3/html/contact-us.html", "Contact", style="font-size:1em; font-weight:normal; font-family:`Noto Sans`; color:white; padding-left:5px; padding-right:5px; border-right:1px solid #4b5e7e;"))
+                  tags$li(a(href="https://www2.gov.bc.ca/gov/content/home", "Home", style="font-size:1em; font-weight:normal; color:white; padding-left:5px; padding-right:5px; border-right:1px solid #4b5e7e;")),
+                  tags$li(a(href="https://www2.gov.bc.ca/gov/content/home/disclaimer", "Disclaimer", style="font-size:1em; font-weight:normal; color:white; padding-left:5px; padding-right:5px; border-right:1px solid #4b5e7e;")),
+                  tags$li(a(href="https://www2.gov.bc.ca/gov/content/home/privacy", "Privacy", style="font-size:1em; font-weight:normal; color:white; padding-left:5px; padding-right:5px; border-right:1px solid #4b5e7e;")),
+                  tags$li(a(href="https://www2.gov.bc.ca/gov/content/home/accessibility", "Accessibility", style="font-size:1em; font-weight:normal; color:white; padding-left:5px; padding-right:5px; border-right:1px solid #4b5e7e;")),
+                  tags$li(a(href="https://www2.gov.bc.ca/gov/content/home/copyright", "Copyright", style="font-size:1em; font-weight:normal; color:white; padding-left:5px; padding-right:5px; border-right:1px solid #4b5e7e;")),
+                  tags$li(a(href="https://www2.gov.bc.ca/StaticWebResources/static/gov3/html/contact-us.html", "Contact", style="font-size:1em; font-weight:normal; color:white; padding-left:5px; padding-right:5px; border-right:1px solid #4b5e7e;"))
                 )
               )
              )
@@ -125,7 +158,7 @@ server <- function(input, output, session) {
   ## select Region.Type, just one
   output$Region.Type <- renderUI({
     selectInput(inputId = "Region.Type",
-                label = "Select a region type:",
+                label = h4("Select a region type"),
                 choices = unique(data1$Region.Type),
                 selected = "Local Health Area"
                 , selectize = FALSE, size = 9    ## forces all 9 options to be shown at once (not drop-down)
@@ -135,7 +168,7 @@ server <- function(input, output, session) {
   ## select Region(s) within selected Region.Type, multiples OK
   output$Region.Name <- renderUI({
     selectInput(inputId = "Region.Name",
-                label = "Select region(s):",
+                label = h4("Select region(s)"),
                 choices = unique(data1$Region.Name[data1$Region.Name == unique(data1$Region.Name)[1]]),
                 multiple = TRUE,
                 selectize = FALSE, size = 7)
@@ -151,7 +184,7 @@ server <- function(input, output, session) {
   ## select Year(s), multiples OK
   output$Year <- renderUI({
     selectInput(inputId = "Year",
-                label = "Select year(s):",
+                label = h4("Select year(s)"),
                 choices = unique(data1$Year),
                 multiple = TRUE,
                 selectize = FALSE, size = 7)
@@ -160,7 +193,7 @@ server <- function(input, output, session) {
   ## select Sex(es), multiples OK
   output$Gender <- renderUI({
     selectInput(inputId = "Gender",
-                label = "Select sex(es):",
+                label = h4("Select gender(s)"),
                 choices = c("Males" = "M", "Females" = "F", "Totals" = "T"),
                 multiple = TRUE,
                 selectize = FALSE, size = 3) ## QQ: Is 4 a minimum? It's ignoring size=3
@@ -169,15 +202,15 @@ server <- function(input, output, session) {
   ## select type of age group, just one
   output$Age_Type <- renderUI({
     radioButtons(inputId = "Age_Type:",
-                 label = "Select type of age group:",
+                 label = NULL,
                  choices = c("Single Year Age Groups", "5-year Age Groups", "Totals", "Custom Age Groups"),
                  selected = "Totals")
   })
 
   ## example table for custom age groups (as text to keep decimals out)
   output$example_table <- renderTable({
-    matrix(data = c("15", "24",  "25", "54",  "55", "64",  "65", "74", "__", "__"),
-           nrow = 5, ncol = 2, byrow = TRUE, dimnames = list(c(1:5), c("From", "To")))
+    matrix(data = c("15", "24",  "25", "54",  "55", "64",  "65", "74"),
+           nrow = 4, ncol = 2, byrow = TRUE, dimnames = list(c(1:4), c("From", "To")))
   })
 
 
@@ -236,8 +269,8 @@ server <- function(input, output, session) {
     if(input$Age_Type == "Custom Age Groups") {
 
       ## 0a. create data frame of custom age groups user typed in
-      custom_ages <- data.frame(S = c(input$start1, input$start2, input$start3, input$start4, input$start5),
-                                E = c(input$end1, input$end2, input$end3, input$end4, input$end5),
+      custom_ages <- data.frame(S = c(input$start1, input$start2, input$start3, input$start4),
+                                E = c(input$end1, input$end2, input$end3, input$end4),
                                 stringsAsFactors = FALSE)
 
       ## 0b. if input (start or end) is decimal,  drop it from calculations.
@@ -304,17 +337,6 @@ server <- function(input, output, session) {
 
       }
 
-      ## 5. if custom age group 5 is not NA, calculate and display its data
-      if(!is.na(custom_ages$S[5]) & !is.na(custom_ages$E[5])){
-
-        A5 <- c(paste0(min(custom_ages$S[5], custom_ages$E[5]), " - ", max(custom_ages$S[5], custom_ages$E[5])))
-        df <- df %>%
-          mutate(
-            !!A5 := rowSums(data1[which(names(data1) == custom_ages$S[5]):
-                                    which(names(data1) == custom_ages$E[5])], dims = 1))
-
-      }
-
       ## put Total column at end
       df <- df %>%
         mutate(Total2 = Total) %>%
@@ -337,22 +359,39 @@ server <- function(input, output, session) {
   })
 
 
-  output$table <- renderDataTable({
+  # output$table <- renderDataTable({
+  # 
+  #   ## call function to create specified data table
+  #   data_df()
+  # 
+  #   },
+  #   ## table options: https://shiny.rstudio.com/articles/datatables.html
+  #   options = list(
+  #     pageLength = 10,       ## show only X rows/page; https://datatables.net/reference/option/pageLength
+  #     lengthMenu = c(10, 20, 25, 50), ## choices of pageLength to display
+  #     scrollX = TRUE,        ## allows horizontal scrolling; https://datatables.net/reference/option/scrollX
+  #     searchable = FALSE,
+  #     filter = FALSE
+  #   )
+  # )
 
-    ## call function to create specified data table
-    data_df()
-
+  output$table <- DT::renderDataTable(datatable({
+    
+      ## call function to create specified data table
+      data_df()
+      
     },
+    filter="none",
     ## table options: https://shiny.rstudio.com/articles/datatables.html
     options = list(
       pageLength = 10,       ## show only X rows/page; https://datatables.net/reference/option/pageLength
       lengthMenu = c(10, 20, 25, 50), ## choices of pageLength to display
       scrollX = TRUE,        ## allows horizontal scrolling; https://datatables.net/reference/option/scrollX
-      searchable = FALSE
+      dom ="ltpi"
     )
   )
-
-
+  )
+  
   output$downloadData <- downloadHandler(
 
     filename = function() {
