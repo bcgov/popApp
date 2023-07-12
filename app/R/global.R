6ac04a33dc37b5d01f3d2204a0aa25178eb8f929
stephanieyurchak <Stephanie.Yurchak@gov.bc.ca>
2023-07-12 11:39:54 -0700

    moved initial app setup to global

--- app/R/global.R
+++ app/R/global.R
@@ -11,7 +11,45 @@
 # See the License for the specific language governing permissions and limitations under the License.
 
 
-#Sourcing weighted median function
+# load libraries  ----
+## installs any missing packages this script uses
+if (!require('tidyverse')) install.packages('tidyverse')
+if (!require('shiny')) install.packages('shiny')
+if (!require('shinydashboard')) install.packages('shinydashboard')
+if (!require('rsconnect')) install.packages('rsconnect')
+if (!require('DT')) install.packages('DT')
+if (!require('GAlogger')) devtools::install_github("bnosac/GAlogger")
+if (!require('markdown')) install.packages('markdown')
+if (!require('scales')) install.packages('scales')
+if (!require('bcdata')) install.packages('bcdata')
+if (!require('bcsapps')) remotes::install_github('bcgov/bcsapps')
+
+## metadata for app ----
+switch_wording <- "Estimates above, Projections below"  ## text in Years selection AFTER switch-year
+
+## Google Analytics ----
+GAlogger::ga_set_tracking_id("UA-150850915-1")
+GAlogger::ga_set_approval(consent = TRUE)
+GAlogger::ga_collect_pageview(page = "/popApp")
+
+## read data ----
+
+## get data from bcdc
+# resource_codes <-  bcdata::bcdc_tidy_resources("86839277-986a-4a29-9f70-fa9b1166f6cb") %>% pull(id)
+# 
+# data1 <- map_dfr(
+#  resource_codes,
+#  bcdata::bcdc_get_data,
+#  record = "86839277-986a-4a29-9f70-fa9b1166f6cb",
+#  show_col_types = FALSE
+# )
+data1 <- readRDS("data/data1.rds")  ## by single-year intervals
+
+## initial selection box values
+## c(Region.Type, Region.Name, Year, Gender)
+initVals <- list("Local Health Area", "British Columbia", max(data1$Year), list("M", "F", "T"))
+
+## weighted median function ----
 median_pop <- function(a, p, t){
   #Consumes the vectors of the same length that represent the age, the 
   #population by age, and the total population. It returns the median as 
